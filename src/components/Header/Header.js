d8b08bdecba86083d4a628321aa424cf581f166d
Kyle Wong <kylewong2510@gmail.com>
2021-04-03 12:02:11 -0600

    Basic Layout component layout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import './Header.css'
 
 const Header = () => {
   return (
     <header>
       <h1>Study Up!</h1>
-      <p>Flashcard Game</p>
+      <p>A Flashcard Game</p>
     </header>
   )
 }
