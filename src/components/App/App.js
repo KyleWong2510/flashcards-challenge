e884dc63d7c337c6caed03fae812e4886a845a37
Kyle Wong <kylewong2510@gmail.com>
2021-04-03 13:45:30 -0600

    Set up Redux store

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Layout from '../Layout/Layout'
 import Form from '../Form/Form'
+import CardContainer from '../CardContainer/CardContainer'
 
 function App() {
   return (
     <div className="App">
-      <Layout>
+      {/* <Layout>
         <Form />
+      </Layout> */}
+      <Layout>
+        <CardContainer />
       </Layout>
     </div>
   );
---
+++ src/components/CardContainer/CardContainer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from '../Card/Card'
+
+const CardContainer = () => {
+  
+  return (
+    <section>
+      
+    </section>
+  )
+}
+
+export default CardContainer
